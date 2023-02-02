ef4adf1ce79fa4d180851034fd2e571af3085e00
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2023-02-02 01:47:47 +0100

    Fixed model loading

---
+++ symai/backend/services/huggingface_client.py
@@ -0,0 +1,20 @@
+import rpyc
+import json
+import os
+from box import Box
+
+
+# check if huggingface mdoel is already initialized
+_hf_config_path_ = os.path.join(os.getcwd(), 'huggingface_seq2seqlm.config.json')
+if not os.path.exists(_hf_config_path_):
+    _parent_dir_ = os.path.dirname(__file__)
+    _hf_config_path_ = os.path.join(_parent_dir_, 'configs', 'huggingface_seq2seqlm.config.json')
+# read the huggingface.config.json file
+with open(_hf_config_path_, 'r') as f:
+    _args_ = Box(json.load(f))
+
+
+if __name__ == '__main__':
+    connection = rpyc.connect('localhost', _args_.port)
+    connection._config['sync_request_timeout'] = 1000
+    connection.root.predict()
