0269843e41969468aaaf7b172dde795bf8ae6c8c
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2023-02-13 04:29:28 +0100

    Added summarizer to extended lib

---
+++ symai/extended/summarizer.py
@@ -0,0 +1,25 @@
+from symai import *
+
+
+class Summarizer(Expression):
+    def __init__(self, filters: List[Expression] = []):
+        super().__init__()
+        filters = filters if isinstance(filters, List) or isinstance(filters, tuple) else [filters]
+        self.data_stream = Stream(Sequence(
+            Clean(),
+            Translate(),
+            Outline(),
+            *filters,
+        ))
+        
+    def forward(self, sym: Symbol, **kwargs) -> Symbol:
+        vals = list(self.data_stream(sym, **kwargs))
+        if len(vals) == 1:
+            res = {str(vals[0]): Expression(vals[0]).compose(**kwargs)}
+            return self._sym_return_type(res)
+        res = Expression(vals).cluster()
+        sym = res.map()
+        summary = {}
+        for k, v in sym.value.items():
+            summary[k] = Expression(v).compose(**kwargs)
+        return self._sym_return_type(summary)
