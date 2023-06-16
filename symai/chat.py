20df774d73cef412e7a0c30e8317eed3ea3057fa
leoentersthevoid <c.leov@icloud.com>
2023-06-16 23:54:50 +0300

    some progress with the recall capability

--- symai/chat.py
+++ symai/chat.py
@@ -1,3 +1,4 @@
+import logging
 import re
 from typing import Any, Optional
 
@@ -8,12 +9,15 @@ from .core import *
 from .memory import Memory, SlidingWindowListMemory, VectorDatabaseMemory
 from .post_processors import ConsolePostProcessor, StripPostProcessor
 from .pre_processors import ConsoleInputPreProcessor
-from .prompts import SymbiaCapabilities
+from .prompts import SymbiaCapabilities, MemoryCapabilities
 from .symbol import Expression, Symbol
 
 
+logging.getLogger('charset_normalizer').setLevel(logging.ERROR)
+
+
 class ChatBot(Expression):
-    _symai_chat: str = """This is a conversation between a chatbot ({}:) and a human (User:). It also includes narration Text (Narrator:) describing the next dialog. The chatbot primarily follows the narrative instructions, and then uses the user input to condition on for the generated response.\n"""
+    _symai_chat: str = """This is a conversation between a chatbot ({}:) and a human (User:). It also includes narration describing the dialog. The chatbot primarily follows the narrative instructions, and then uses the user input to condition on for the generated response. If the chatbot has retrieved any long-term memories, it checks the user query and narrates based on the long-term memory buffer."""
 
     def __init__(self, value = None, name: str = 'Symbia', output: Optional[Output] = None, verbose: bool = False):
         super().__init__(value)
@@ -21,54 +25,62 @@ class ChatBot(Expression):
         self.name                 = name
         self.last_user_input: str = ''
 
-        self.memory:           Memory = SlidingWindowListMemory()
-        self.long_term_memory: Memory = VectorDatabaseMemory(enabled=settings.SYMAI_CONFIG['INDEXING_ENGINE_API_KEY'] is not None)
+        self.short_term_memory: Memory = SlidingWindowListMemory(window_size=10)
+        self.long_term_memory:  Memory = VectorDatabaseMemory(enabled=settings.SYMAI_CONFIG['INDEXING_ENGINE_API_KEY'] is not None, top_k=10)
 
         self._preprocessor  = ChatBot._init_custom_input_preprocessor(name=name, that=self)
         self._postprocessor = ChatBot._init_custom_input_postprocessor(that=self)
 
-        self.classify = InContextClassification(SymbiaCapabilities())
+        self.detect_capability   = InContextClassification(SymbiaCapabilities())
+        self.detect_memory_usage = InContextClassification(MemoryCapabilities())
+
         Expression.command(engines=['symbolic'], expression_engine='wolframalpha')
 
     def repeat(self, query, **kwargs):
         return self.narrate('Symbia does not understand and asks to repeat and give more context.', prompt=query)
 
-    def narrate(self, message: str, context: str = None, end: bool = False, do_recall: bool = True, **kwargs) -> Symbol:
-        narration = f'Narrator: {message}'
-        self.memory.store(narration)
+    def narrate(self, message: str, context: str, category: str = None, end: bool = False, **kwargs) -> Symbol:
+        reflection = context if context is not None else ''
+        ltmem_recall = 'No memories retrieved.'
+        stmem_recall = 'No memories retrieved.'
 
-        ctxt    = context if context is not None else ''
-        value   = f"{self.static_context}ADDITIONAL FACTS AND CONTEXT\n{ctxt}\n" if ctxt is not None else ''
-        value  += 'SHORT-TERM MEMORY RECALL\n'
-        value  += '\n'.join(self.memory.recall()) # TODO: use vector search DB
-        value  += '\n\nLONG-TERM MEMORY RECALL (Consider only if relevant to the user query!)\n\n'
-        query   = f'{self.last_user_input}\n{message}\n\n'
-        recall  = self.long_term_memory.recall(query) if do_recall else []
-        value  += '\n'.join(recall)
-        value  += f'\n{self.name}:'
+        if self.verbose:
+            logging.debug(f'Narration:\n{message}\n')
+            logging.debug(f'Reflection:\n{reflection}\n')
 
-        if self.verbose: print('[DEBUG] long-term memory recall: ', recall)
-        if self.verbose: print('[DEBUG] narration: ', narration)
-        if self.verbose: print('[DEBUG] function context: ', ctxt)
+        if category == 'RECALL':
+            stmem_recall = '\n'.join(self.short_term_memory.recall())
+            stmem_recall = stmem_recall if len(stmem_recall) > 0 else 'No memories retrieved.'
+            ltmem_recall = '\n'.join(self.long_term_memory.recall(reflection))
+            ltmem_recall = ltmem_recall if len(ltmem_recall) > 0 else 'No memories retrieved.'
+            scratchpad   = self._memory_scratchpad(reflection, stmem_recall, ltmem_recall)
+            memory_usage = str(self.detect_memory_usage(scratchpad))
 
-        @zero_shot(prompt=value, stop=['User:'], **kwargs)
-        def _func(_) -> str:
-            pass
-        model_rsp = _func(self)
+            if self.verbose:
+                logging.debug(f'Scratchpad:\n{scratchpad}\n')
+                logging.debug(f'Memory usage:\n{memory_usage}\n')
+                logging.debug(f'Retrieved from short-term memory:\n{stmem_recall}\n')
+                logging.debug(f'Retrieved from long-term memory:\n{ltmem_recall}\n')
 
-        rsp = f"{self.name}: {model_rsp}"
-        if self.verbose: print('[DEBUG] model reply: ', model_rsp)
-        memory = f"{self.last_user_input}" if do_recall else ""
-        if len(memory) > 0: self.long_term_memory.store(memory)
-        memory = f"{model_rsp}" if do_recall else ""
-        if len(memory) > 0: self.long_term_memory.store(memory)
-        if self.verbose: print('[DEBUG] store new memory reply: ', memory)
+            do         = self._extract_category(memory_usage)
+            reflection = self._extract_reflection(memory_usage)
 
-        sym = Symbol(rsp)
+            if do == 'SAVE':
+                self.long_term_memory.store(f'{self.name}: {reflection}')
+                if self.verbose: logging.debug(f'Store new long-term memory:\n{reflection}\n')
+                message = f'{self.name} inform the user that the memory was stored.'
+            elif do == 'DUPLICATE':
+                message = f'{self.name} engages the user in a conversation about the duplicate topic, showing the user she remembered the past interaction.'
+            elif do == 'IRRELEVANT':
+                message = f'{self.name} discusses the topic with the user.'
 
-        if end: print(sym)
+        if self.verbose:
+            logging.debug(f'Storing new short-term memory:\nUser: {self.last_user_input}\n')
+            logging.debug(f'Storing new short-term memory:\n{self.name}: {reflection}\n')
 
-        return sym
+        reply = f'{self.name}: {self._narration(message, self.last_user_input, reflection, ltmem_recall, stmem_recall, **kwargs)}'
+
+        return Symbol(reply)
 
     def input(self, message: str = "Please add more information", **kwargs) -> Symbol:
         @userinput(
@@ -99,8 +111,10 @@ class ChatBot(Expression):
                 if f'{name}:' in Symbol(args[0]): input_ = f'\n{str(args[0])}\n$> '
                 else:                             input_ = f'\n{name}: {str(args[0])}\n$> '
 
-                that.memory.store(input_)
-                that.long_term_memory.store(str(args[0]))
+                memory = args[0].value.split(name+':')[-1]
+                memory = memory.split('(')[-1][:-1] #@NOTE: equivalent to self._extract_reflection
+                if len(memory) > 0:
+                    that.short_term_memory.store(f'{name}:' + memory)
 
                 return input_
 
@@ -110,7 +124,7 @@ class ChatBot(Expression):
     def _init_custom_input_postprocessor(that):
         class CustomInputPostProcessor(ConsolePostProcessor):
             def __call__(self, wrp_self, wrp_params, rsp, *args, **kwargs):
-                that.memory.store(f'User: {str(rsp)}')
+                that.short_term_memory.store(f'User: {str(rsp)}')
 
                 return rsp
 
@@ -118,99 +132,155 @@ class ChatBot(Expression):
 
 class SymbiaChat(ChatBot):
     def forward(self):
-        message = self.narrate(f'{self.name} introduces herself, writes a greeting message and asks how to help.', do_recall=False)
+        message = self.narrate(f'{self.name} introduces herself, writes a greeting message and asks how to help.', context=None)
 
         while True:
             usr = self.input(message)
             self.last_user_input = usr
-            if self.verbose: print('[DEBUG] user query: ', usr)
+            if self.verbose: logging.debug(f'User:\n{usr}\n')
 
             if len(str(usr)) > 0:
-                ctxt = str(self.classify(usr))
+                ctxt = str(self.detect_capability(usr))
             else:
                 ctxt = '[DK]'
 
-            if self.verbose: print('[DEBUG] context: ', ctxt)
+            if self.verbose: logging.debug(f'In-context:\n{ctxt}\n')
 
             if '[EXIT]' in ctxt:
-                self.narrate(f'{self.name} writes goodbye message.', end=True, do_recall=False)
+                self.narrate(f'{self.name} writes friendly goodbye message.', context=None, end=True)
                 break
 
             elif '[HELP]' in ctxt:
-                thought = self._extract_thought(ctxt)
-                message = self.narrate(f'{self.name} ', context=thought, do_recall=False)
+                reflection = self._extract_reflection(ctxt)
+                message    = self.narrate(f'{self.name} ', context=reflection)
+
+            elif '[RECALL]' in ctxt:
+                reflection = self._extract_reflection(ctxt)
+                category   = self._extract_category(ctxt)
+                message    = self.narrate(f'{self.name} uses replies based on what has been recovered from the memory.', context=ctxt, category=category)
 
             elif '[DK]' in ctxt:
-                thought = self._extract_thought(ctxt)
-                q = usr.extract('user query request')
-                rsp = self.search(q)
-                message = self.narrate(f'{self.name} is not sure about the message and references to search results.', context=thought, payload=rsp)
+                reflection = self._extract_reflection(ctxt)
+                message    = self.narrate(f'{self.name} is not sure about the message and references and asks the user for more context.', context=reflection)
 
             else:
                 try:
                     if '[SYMBOLIC]' in ctxt:
-                        q = usr.extract("mathematical formula that WolframAlpha can solve")
-                        rsp = q.expression()
-                        thought = self._extract_thought(ctxt)
+                        q       = usr.extract("mathematical formula that WolframAlpha can solve")
+                        rsp     = q.expression()
                         message = self.narrate(f'{self.name} replies to the user and provides the solution of the math problem.', context=rsp)
 
                     if '[SEARCH]' in ctxt:
-                        q = usr.extract('user query request')
-                        rsp = self.search(q)
-                        thought = self._extract_thought(ctxt)
+                        q       = usr.extract('user query request')
+                        rsp     = self.search(q)
                         message = self.narrate(f'{self.name} replies to the user based on the online search results.', context=rsp)
 
                     elif '[CRAWLER]' in ctxt:
-                        q = usr.extract('URL from text')
-                        q = q.convert('proper URL, example: https://www.google.com')
-                        site = self.fetch(q)
+                        q       = usr.extract('URL from text')
+                        q       = q.convert('proper URL, example: https://www.google.com')
+                        site    = self.fetch(q)
                         site.save('tmp.html')
-                        thought = self._extract_thought(ctxt)
                         message = self.narrate(f'{self.name} explains that the website is downloaded to the `tmp.html` file.')
 
                     elif '[SPEECH-TO-TEXT]' in ctxt:
-                        q = usr.extract('extract file path')
-                        rsp = self.speech(q)
-                        thought = self._extract_thought(ctxt)
+                        q       = usr.extract('extract file path')
+                        rsp     = self.speech(q)
                         message = self.narrate('Symbia replies to the user and transcribes the content of the audio file.', context=rsp)
 
                     elif '[TEXT-TO-IMAGE]' in ctxt:
-                        q = usr.extract('text for image creation')
-                        rsp = q.draw()
-                        thought = self._extract_thought(ctxt)
+                        q       = usr.extract('text for image creation')
+                        rsp     = q.draw()
                         message = self.narrate('Symbia replies to the user and provides the image URL.', context=rsp)
 
                     elif '[OCR]' in ctxt:
-                        url = usr.extract('extract url')
-                        rsp = Expression().ocr(url)
-                        thought = self._extract_thought(ctxt)
-                        message = self.narrate('Symbia replies to the user and provides OCR text from the image.',
-                                                context=rsp)
+                        url     = usr.extract('extract url')
+                        rsp     = Expression().ocr(url)
+                        message = self.narrate('Symbia replies to the user and provides OCR text from the image.', context=rsp)
 
                     elif '[RETRIEVAL]' in ctxt:
                         file = usr.extract('extract file path')
-                        q = usr.extract('user question')
-                        rsp = file.fstream(
+                        q    = usr.extract('user question')
+                        rsp  = file.fstream(
                             Sequence(
                                 IncludeFilter('include only facts related to the user question: ' @ q),
                                 Outline()
                             )
                         )
-                        thought = self._extract_thought(ctxt)
                         message = self.narrate('Symbia replies to the user and outlines and relies to the user query.', context=rsp)
 
                     else:
-                        q = usr.extract('user query request')
-                        rsp = self.search(q)
-                        thought = self._extract_thought(ctxt)
-                        message = self.narrate('Symbia tries to interpret the response, and if unclear asks the user to restate the statement or add more context.', context=thought)
+                        q          = usr.extract('user query request')
+                        rsp        = self.search(q)
+                        reflection = self._extract_reflection(ctxt)
+                        message    = self.narrate('Symbia tries to interpret the response, and if unclear asks the user to restate the statement or add more context.', context=reflection)
 
                 except Exception as e:
-                    thought = self._extract_thought(ctxt)
-                    message = self.narrate('Symbia apologizes and explains the user what went wrong.', context=str(e), do_recall=False)
+                    reflection = self._extract_reflection(ctxt)
+                    message = self.narrate('Symbia apologizes and explains the user what went wrong.', context=str(e))
 
-    def _extract_thought(self, msg: str) -> str:
-        return re.findall(r'\{([^}]+)\}', msg).pop()
+    def _extract_reflection(self, msg: str) -> str:
+        res = re.findall(r'\(([^)]+)\)', msg)
+        if len(res) > 0:
+            return res.pop()
+        return
+
+    def _extract_category(self, msg: str) -> str:
+        res = re.findall(r'\[([^]]+)\]', msg)
+        if len(res) > 0:
+            return res.pop()
+        return
+
+    def _memory_scratchpad(self, context, short_term_memory, long_term_memory):
+        scratchpad = f'''
+[REFLECT](
+    Query:      {self.last_user_input}
+    Reflection: {context}
+)
+
+[SHORT-TERM MEMORY RECALL](
+    {short_term_memory}
+)
+
+[LONG-TERM MEMORY RECALL](
+    {long_term_memory}
+)
+'''
+
+        return scratchpad
+
+    def _narration(self, msg: str, query: str, reply: str, ltmem_recall: str, stmem_recall: str, **kwargs):
+        prompt = f'''
+{self._symai_chat}
+
+[NARRATION](
+    {msg}
+)
+
+[LONG-TERM MEMORY RECALL](
+    {ltmem_recall}
+)
+
+[SHORT-TERM MEMORY RECALL](
+    {stmem_recall}
+)
+
+[USER](
+    {query}
+)
+
+[REPLY](
+    {reply}
+)
+'''
+
+        @zero_shot(prompt=prompt, **kwargs)
+        def _func(_) -> str:
+            pass
+
+        if self.verbose: logging.debug(f'Narration:\n{prompt}\n')
+
+        return _func(self)
+
 
 def run() -> None:
     chat = SymbiaChat()
