4b73edf1eb5e38464b135f1038a7c893b151c6a3
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2023-07-04 18:09:10 +0200

    Add missing packages file

---
+++ symai/extended/packages/sympkg.py
@@ -0,0 +1,77 @@
+import sys
+import argparse
+import os
+from symai.imports import Import
+
+
+class PackageHandler():
+    def __init__(self):
+        self.home_dir = os.path.expanduser("~")
+        self.package_dir = os.path.join(self.home_dir, "symai-packages")
+
+        if not os.path.exists(self.package_dir):
+            os.makedirs(self.package_dir)
+
+        os.chdir(self.package_dir)
+
+        parser = argparse.ArgumentParser(
+            description='''SymbolicAI package manager.
+            Manage extensions from the command line.
+            You can (i) install, (r) remove, (l) list installed, or (u) update a module.''',
+            usage='''sympkg <command> [<args>]
+
+            The most commonly used sympkg commands are:
+            i   Install a new package
+            r   Remove an installed package
+            l   List all installed packages
+            u   Update an installed package
+            '''
+        )
+
+        parser.add_argument('command', help='Subcommand to run')
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            print('Unrecognized command')
+            parser.print_help()
+            exit(1)
+
+        getattr(self, args.command)()
+
+    def i(self):
+        parser = argparse.ArgumentParser(
+            description='Install a new package',
+            usage='sympkg i [package]'
+        )
+        parser.add_argument('package', help='Name of package to install')
+        args = parser.parse_args(sys.argv[2:])
+        Import.install(args.package)
+
+    def r(self):
+        parser = argparse.ArgumentParser(
+            description='Remove an installed package',
+            usage='sympkg r [package]'
+        )
+        parser.add_argument('package', help='Name of package to remove')
+        args = parser.parse_args(sys.argv[2:])
+        Import.remove(args.package)
+
+    def l(self):
+        import pprint
+        pprint.pprint(Import.list_installed())
+
+    def u(self):
+        parser = argparse.ArgumentParser(
+            description='Update an installed package',
+            usage='sympkg u [package]'
+        )
+        parser.add_argument('package', help='Name of package to update')
+        args = parser.parse_args(sys.argv[2:])
+        Import.update(args.package)
+
+
+def run() -> None:
+    PackageHandler()
+
+
+if __name__ == '__main__':
+    run()
