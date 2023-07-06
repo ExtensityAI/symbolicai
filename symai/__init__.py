064bd2fd4d04f79c570ce3ae7b9429899247db1e
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-07-06 02:29:11 +0200

    new version

--- symai/__init__.py
+++ symai/__init__.py
@@ -9,7 +9,7 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("tika").setLevel(logging.ERROR)
 
 
-SYMAI_VERSION = "0.2.34"
+SYMAI_VERSION = "0.2.35"
 __version__   = SYMAI_VERSION
 __root_dir__  = Path.home() / '.symai'
 
