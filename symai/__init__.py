e2735314bf88b23c681cce6767db15f8e6cf51a2
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-07-06 03:19:47 +0200

    Fixed package install

--- symai/__init__.py
+++ symai/__init__.py
@@ -9,7 +9,7 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("tika").setLevel(logging.ERROR)
 
 
-SYMAI_VERSION = "0.2.35"
+SYMAI_VERSION = "0.2.36"
 __version__   = SYMAI_VERSION
 __root_dir__  = Path.home() / '.symai'
 
