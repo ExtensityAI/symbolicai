41727e2b5d41fff7611bb1628cef8894764ec922
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-11-02 18:39:56 +0100

    updated for new minor release version

--- symai/__init__.py
+++ symai/__init__.py
@@ -9,7 +9,7 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("tika").setLevel(logging.ERROR)
 
 
-SYMAI_VERSION = "0.2.55"
+SYMAI_VERSION = "0.2.56"
 __version__   = SYMAI_VERSION
 __root_dir__  = Path.home() / '.symai'
 
