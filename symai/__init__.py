1ab4ffca44c61ed35a0e5f0edc3912c4e9da5040
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-11-06 13:45:38 +0100

    Added minor tweaks

--- symai/__init__.py
+++ symai/__init__.py
@@ -9,7 +9,7 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("tika").setLevel(logging.ERROR)
 
 
-SYMAI_VERSION = "0.2.61"
+SYMAI_VERSION = "0.2.62"
 __version__   = SYMAI_VERSION
 __root_dir__  = Path.home() / '.symai'
 
