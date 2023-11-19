2e452361e3d7c03e3f4a10b7a663558a3f59a909
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-11-19 19:31:53 +0100

    Added working api endpoint for components

--- symai/endpoints/api.py
+++ symai/endpoints/api.py
@@ -1,54 +1,61 @@
+import importlib
 from fastapi import FastAPI, APIRouter, Body
 from pydantic import BaseModel, create_model
 from typing import Any, Dict, List, Optional, Union, Type
 from .. import Symbol, Expression
 from ..components import Translate, Compose
 
-# Placeholder import statements for Symbol and Expression
-# These should be replaced by actual import statements
-# from your_symbol_module import Symbol
-# from your_expression_module import Expression
 
 app = FastAPI(title="SymbolicAI API", version="1.0")
+
 
 class CreateSymbolRequest(BaseModel):
     value: Optional[Any] = None
     static_context: Optional[str] = ''
 
+
 class CreateExpressionRequest(BaseModel):
     value: Optional[Any] = None
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello Symbols"}
+
 
 @app.post("/symbol/")
 def create_symbol(symbol_request: CreateSymbolRequest):
     symbol = Symbol(symbol_request.value, static_context=symbol_request.static_context)
     return symbol.__dict__
 
+
 @app.post("/expression/")
 def create_expression(expression_request: CreateExpressionRequest):
     expression = Expression(expression_request.value)
     return expression.__dict__
 
+
 @app.get("/symbol/{symbol_id}/")
 def get_symbol(symbol_id: int):
     pass  # Implement retrieval logic
+
 
 @app.get("/expression/{expression_id}/")
 def get_expression(expression_id: int):
     pass  # Implement retrieval logic
 
+
 class SymbolMethodRequest(BaseModel):
     args: List[Any] = []
     kwargs: Dict[str, Any] = {}
+
 
 @app.post("/symbol/{symbol_id}/call/")
 def call_symbol(symbol_id: int, method_request: SymbolMethodRequest):
     symbol = Symbol()  # Replace with retrieval logic
     result = symbol(*method_request.args, **method_request.kwargs)
     return {"result": result}
+
 
 @app.post("/symbol/{symbol_id}/{method_name}/")
 def operate_on_symbol(symbol_id: int, method_name: str, method_request: SymbolMethodRequest):
@@ -59,6 +66,7 @@ def operate_on_symbol(symbol_id: int, method_name: str, method_request: SymbolMe
     result = method(*method_request.args, **method_request.kwargs)
     return {"result": result.__dict__ if isinstance(result, Symbol) else result}
 
+
 @app.post("/expression/{expression_id}/{method_name}/")
 def operate_on_expression(expression_id: int, method_name: str, method_request: SymbolMethodRequest):
     expression = Expression()  # Replace with retrieval logic
@@ -68,12 +76,15 @@ def operate_on_expression(expression_id: int, method_name: str, method_request: 
     result = method(*method_request.args, **method_request.kwargs)
     return {"result": result.__dict__ if isinstance(result, Symbol) else result}
 
+
 class BaseSymbolModel(BaseModel):
     value: Any
     static_context: str
 
+
 class BaseExpressionModel(BaseModel):
     value: Any
+
 
 def create_symbol_model(symbol_cls: Type[Symbol]) -> Type[BaseModel]:
     fields = {
@@ -83,6 +94,7 @@ def create_symbol_model(symbol_cls: Type[Symbol]) -> Type[BaseModel]:
     symbol_model = create_model(f'{symbol_cls.__name__}Model', **fields, __base__=BaseSymbolModel)
     return symbol_model
 
+
 def create_expression_model(expression_cls: Type[Expression]) -> Type[BaseModel]:
     fields = {
         "value": (Any, ...)
@@ -90,44 +102,40 @@ def create_expression_model(expression_cls: Type[Expression]) -> Type[BaseModel]
     expression_model = create_model(f'{expression_cls.__name__}Model', **fields, __base__=BaseExpressionModel)
     return expression_model
 
+
 router = APIRouter()
 
-def create_class_endpoint(cls: Union[Type[Symbol], Type[Expression]], cls_model: BaseModel):
-    def create_instance(data: cls_model = Body(...)):
-        data_dict = data.dict()
-        expected_args = cls.__init__.__annotations__.keys()
-        filtered_data = {k: v for k, v in data_dict.items() if k in expected_args}
-        instance = cls(**filtered_data)
-        return instance.__dict__
 
-    create_instance.__name__ = f"create_{cls.__name__.lower()}_instance"
-    router.post(f"/components/{cls.__name__.lower()}/", response_model=Dict[str, Any])(create_instance)
-
-    methods = [func for func in dir(cls) if callable(getattr(cls, func)) and not func.startswith("_")]
-    for method_name in methods:
-        endpoint = get_endpoint(cls, method_name)
-        router.add_api_route(f"/components/{cls.__name__.lower()}/{method_name}/", endpoint,
-                              methods=["POST"], response_model=Dict[str, Any])
-
-def get_endpoint(cls, method_name):
-    def method(data: Dict[str, Any] = Body(...)):
-        instance = cls()  # Replace with actual instance retrieval logic
-        method_func = getattr(instance, method_name, None)
-        if method_func:
-            result = method_func(**data)
-            return {"result": result.__dict__ if isinstance(result, (Symbol, Expression)) else result}
-        else:
-            return {"error": f"Method {method_name} not found or is not callable."}
-    method.__name__ = method_name  # To avoid name collisions in the APIRouter
-    return method
+# Modify the existing GenericRequest
+class GenericRequest(BaseModel):
+    class_name: str
+    init_args: List[Any]            = {}
+    init_kwargs: Dict[str, Any]     = {}
+    forward_args: List[ Any]        = {}
+    forward_kwargs: Dict[str, Any]  = {}
 
 
-# Automatically generate models and endpoints for each Symbol and Expression subclass
-# Replace Translate and ExampleSymbol with actual subclasses of Symbol and Expression
-subclasses_expression = [Translate, Compose]
+@app.post("/components/")
+async def generic_forward(request: GenericRequest):
+    # Dynamically import the class from components module based on request.class_name
+    components_module = importlib.import_module('.components', package='symai')
+    cls = getattr(components_module, request.class_name)
 
-for subclass in subclasses_expression:
-    expression_model = create_expression_model(subclass)
-    create_class_endpoint(subclass, expression_model)
+    # Check if cls is subclass of Expression and instantiate
+    if not issubclass(cls, components_module.Expression):
+        raise ValueError("The provided class name must be a subclass of Expression")
+
+    # Initialize the class with provided init_args, requiring unpacking **kwargs
+    instance = cls(*request.init_args, **request.init_kwargs)
+
+    # Call the forward method with provided forward_args, requiring unpacking **kwargs
+    result = instance.forward(*request.forward_args, **request.forward_kwargs)
+
+    # Assume result is a components.Symbol instance, you need to return a serializable format
+    if isinstance(result, components_module.Symbol):
+        return result.__dict__
+    else:
+        return result  # If result is already a serializable type
+
 
 app.include_router(router)
