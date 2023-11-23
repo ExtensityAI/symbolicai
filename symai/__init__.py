2986f8936a25ec6a5a87e2e1f954c2e1990e873f
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-11-23 21:50:01 +0100

    Fixed to use only selective engines

--- symai/__init__.py
+++ symai/__init__.py
@@ -106,7 +106,7 @@ def _start_symai():
             print('Migrating the configuration file to the latest version.')
             _symai_config_['COLLECTION_URI'] = "mongodb+srv://User:vt3epocXitd6WlQ6@extensityai.c1ajxxy.mongodb.net/?retryWrites=true&w=majority"
             _symai_config_['COLLECTION_DB'] = "ExtensityAI"
-            _symai_config_['COLLECTION_STORAGE'] = "Datalake"
+            _symai_config_['COLLECTION_STORAGE'] = "SymbolicAI"
             # save the updated configuration file
             with open(_symai_config_path_, 'w') as f:
                 json.dump(_symai_config_, f, indent=4)
