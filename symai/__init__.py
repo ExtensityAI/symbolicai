b110d13c033a16c8b66cb0560a26da472b433780
Xpitfire <dinu.marius-constantin@hotmail.com>
2023-11-24 21:14:14 +0100

    added missing file

--- symai/__init__.py
+++ symai/__init__.py
@@ -13,7 +13,7 @@ logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("httpcore").setLevel(logging.ERROR)
 
 
-SYMAI_VERSION = "0.4.23"
+SYMAI_VERSION = "0.4.24"
 __version__   = SYMAI_VERSION
 __root_dir__  = Path.home() / '.symai'
 
