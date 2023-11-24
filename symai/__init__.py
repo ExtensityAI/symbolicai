7db3b012a9092021f36f0575c0efef0c9df31c6d
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2023-11-24 22:50:54 +0100

    Fixed typo

--- symai/__init__.py
+++ symai/__init__.py
@@ -13,7 +13,7 @@ logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("httpcore").setLevel(logging.ERROR)
 
 
-SYMAI_VERSION = "0.4.32"
+SYMAI_VERSION = "0.4.33"
 __version__   = SYMAI_VERSION
 __root_dir__  = Path.home() / '.symai'
 
