2d18e84ad1ddce0f722afdb31709a4876594eb48
Xpitfire <dinu.marius-constantin@hotmail.com>
2024-08-10 01:30:46 +0200

    Added named expression imports

--- symai/imports.py
+++ symai/imports.py
@@ -7,6 +7,7 @@ import stat
 import subprocess
 import sys
 from pathlib import Path
+from typing import List, Union
 
 from .symbol import Expression
 
@@ -67,6 +68,27 @@ class Import(Expression):
                 relative_module_path = module.split('/')[0] + '.' + module.split('/')[1] + '.' + relative_module_path
                 module_class = getattr(importlib.import_module(relative_module_path), expr['type'])
                 module_classes.append(module_class)
+        return module_classes
+
+    @staticmethod
+    def load_expression(module, expressions: Union[List[str] | str]) -> Union[List[Expression] | Expression]:
+        module_classes = []
+        with open(f'{BASE_PACKAGE_PATH}/{module}/package.json') as f:
+            pkg = json.load(f)
+            for expr in pkg['expressions']:
+                module_path = f'{BASE_PACKAGE_PATH}/{expr["module"].replace("/", ".")}'
+                # Determine relative path and adjust namespace
+                relative_module_path = module_path.replace(BASE_PACKAGE_PATH, '').replace('/', '.').lstrip('.')
+                # Replace with actual username and package_name values
+                relative_module_path = module.split('/')[0] + '.' + module.split('/')[1] + '.' + relative_module_path
+                if isinstance(expressions, str):
+                    if expr['type'] == expressions:
+                        module_class = getattr(importlib.import_module(relative_module_path), expr['type'])
+                        return module_class
+                elif isinstance(expressions, list):
+                    if expr['type'] in expressions:
+                        module_class = getattr(importlib.import_module(relative_module_path), expr['type'])
+                        module_classes.append(module_class)
         return module_classes
 
     def __new__(self, module, auto_clone: bool = True, verbose: bool = False, *args, **kwargs):
