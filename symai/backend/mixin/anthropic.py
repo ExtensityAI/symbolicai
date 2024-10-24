560ec4d6e3c1c49e7fa4af98a10085d35f486827
futurisold <c.leov@icloud.com>
2024-10-24 12:08:25 +0200

    (feat) added latest antrophic models

--- symai/backend/mixin/anthropic.py
+++ symai/backend/mixin/anthropic.py
@@ -1,6 +1,9 @@
 SUPPORTED_MODELS = [
     # https://docs.anthropic.com/en/docs/about-claude/models
+    'claude-3-5-sonnet-latest',
+    'claude-3-5-sonnet-20241022',
     'claude-3-5-sonnet-20240620',
+    'claude-3-opus-latest',
     'claude-3-opus-20240229',
     'claude-3-sonnet-20240229',
     'claude-3-haiku-20240307',
@@ -8,14 +11,20 @@ SUPPORTED_MODELS = [
 
 class AnthropicMixin:
     def api_max_context_tokens(self):
-        if self.model == 'claude-3-5-sonnet-20240620' or \
+        if self.model == 'claude-3-5-sonnet-latest' or \
+           self.model == 'claude-3-5-sonnet-20241022' or \
+           self.model == 'claude-3-5-sonnet-20240620' or \
+           self.model == 'claude-3-opus-latest' or \
            self.model == 'claude-3-opus-20240229' or \
            self.model == 'claude-3-sonnet-20240229' or \
            self.model == 'claude-3-haiku-20240307':
             return 200_000
 
     def api_max_response_tokens(self):
-        if self.model == 'claude-3-5-sonnet-20240620' or \
+        if self.model == 'claude-3-5-sonnet-latest' or \
+           self.model == 'claude-3-5-sonnet-20240620' or \
+           self.model == 'claude-3-5-sonnet-20241022' or \
+           self.model == 'claude-3-opus-latest' or \
            self.model == 'claude-3-opus-20240229' or \
            self.model == 'claude-3-sonnet-20240229' or \
            self.model == 'claude-3-haiku-20240307':
