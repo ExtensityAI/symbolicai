522a26de09bf007e2babbcc690c755070a53a1a3
futurisold <leo@extensity.ai>
2025-03-20 16:12:27 +0200

    (feat) enabled logprobs/top_logprobs for openai

--- symai/backend/engines/neurosymbolic/engine_openai_gptX_reasoning.py
+++ symai/backend/engines/neurosymbolic/engine_openai_gptX_reasoning.py
@@ -407,4 +407,6 @@ class GPTXReasoningEngine(Engine, OpenAIMixin):
             "tools": kwargs.get('tools'),
             "tool_choice": kwargs.get('tool_choice'),
             "response_format": kwargs.get('response_format'),
+            "logprobs": kwargs.get('logprobs'),
+            "top_logprobs": kwargs.get('top_logprobs'),
         }
