e56819a58c1ad57e7e54ab396bfa992b57d3e09b
futurisold <leo@extensity.ai>
2025-05-03 00:10:55 +0300

    (feat) new release

--- symai/__init__.py
+++ symai/__init__.py
@@ -35,7 +35,7 @@ os.environ['TOKENIZERS_PARALLELISM'] = "false"
 # Create singleton instance
 config_manager = settings.SymAIConfig()
 
-SYMAI_VERSION = "0.9.3"
+SYMAI_VERSION = "0.9.4"
 __version__   = SYMAI_VERSION
 __root_dir__  = config_manager.config_dir
 
