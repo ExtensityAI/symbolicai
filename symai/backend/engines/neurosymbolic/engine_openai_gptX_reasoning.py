4027129012127049c64a7a08a0156eae0025547c
futurisold <leo@extensity.ai>
2025-05-24 18:31:01 +0300

    (chore) minor refactoring

--- symai/backend/engines/neurosymbolic/engine_openai_gptX_reasoning.py
+++ symai/backend/engines/neurosymbolic/engine_openai_gptX_reasoning.py
@@ -230,9 +230,8 @@ class GPTXReasoningEngine(Engine, OpenAIMixin):
                 raise e
 
         metadata = {'raw_output': res}
+        output = [r.message.content for r in res.choices]
 
-        rsp    = [r.message.content for r in res.choices]
-        output = rsp if isinstance(messages, list) else rsp[0]
         return output, metadata
 
     def prepare(self, argument):
