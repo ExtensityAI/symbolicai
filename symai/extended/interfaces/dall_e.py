3fcec4deb1e45e3d62407230c6d1c203d820fa86
futurisold <leo@extensity.ai>
2025-07-28 02:32:10 +0300

    (fix) dall e interface

--- symai/extended/interfaces/dall_e.py
+++ symai/extended/interfaces/dall_e.py
@@ -1,6 +1,6 @@
 from ... import core
 from ...symbol import Expression, Symbol
-from ...backend.engines.drawing.engine_dall_e import DalleResult
+from ...backend.engines.drawing.engine_gpt_image import GPTImageResult
 
 
 class dall_e(Expression):
@@ -8,9 +8,9 @@ class dall_e(Expression):
         super().__init__(*args, **kwargs)
         self.name = self.__class__.__name__
 
-    def __call__(self, sym: Symbol, operation: str = 'create', **kwargs) -> DalleResult:
+    def __call__(self, sym: Symbol, operation: str = 'create', **kwargs) -> GPTImageResult:
         sym = self._to_symbol(sym)
         @core.draw(operation=operation, **kwargs)
-        def _func(_) -> DalleResult:
+        def _func(_) -> GPTImageResult:
             pass
         return _func(sym)
