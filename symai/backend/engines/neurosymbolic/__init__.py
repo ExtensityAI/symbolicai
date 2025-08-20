4b960a20c92a08a2586230c3fd106243c6a6e68f
futurisold <leo@extensity.ai>
2025-08-21 01:31:24 +0300

    (feat) enabled groq engine token tracking; enabled groq dynamic engine; fixed bug in runtime info obj

--- symai/backend/engines/neurosymbolic/__init__.py
+++ symai/backend/engines/neurosymbolic/__init__.py
@@ -1,11 +1,13 @@
 from ...mixin import (ANTHROPIC_CHAT_MODELS, ANTHROPIC_REASONING_MODELS,
                       DEEPSEEK_CHAT_MODELS, DEEPSEEK_REASONING_MODELS,
                       GOOGLE_CHAT_MODELS, GOOGLE_REASONING_MODELS,
+                      GROQ_CHAT_MODELS, GROQ_REASONING_MODELS,
                       OPENAI_CHAT_MODELS, OPENAI_REASONING_MODELS)
 from .engine_anthropic_claudeX_chat import ClaudeXChatEngine
 from .engine_anthropic_claudeX_reasoning import ClaudeXReasoningEngine
 from .engine_deepseekX_reasoning import DeepSeekXReasoningEngine
 from .engine_google_geminiX_reasoning import GeminiXReasoningEngine
+from .engine_groq import GroqEngine
 from .engine_openai_gptX_chat import GPTXChatEngine
 from .engine_openai_gptX_reasoning import GPTXReasoningEngine
 
@@ -17,4 +19,6 @@ ENGINE_MAPPING = {
     **{model_name: GeminiXReasoningEngine for model_name in GOOGLE_REASONING_MODELS},
     **{model_name: GPTXChatEngine for model_name in OPENAI_CHAT_MODELS},
     **{model_name: GPTXReasoningEngine for model_name in OPENAI_REASONING_MODELS},
+    **{model_name: GroqEngine for model_name in GROQ_CHAT_MODELS},
+    **{model_name: GroqEngine for model_name in GROQ_REASONING_MODELS},
 }
